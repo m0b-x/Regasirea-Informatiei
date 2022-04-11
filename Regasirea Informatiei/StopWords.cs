1a8e44d829ce6cce222aecd0c3345a957b0f51f5
m0b-x <mobx3000@yahoo.com>
2022-04-11 18:34:05 +0300

    Adaugat Clasa StopWords.cs

---
+++ Regasirea Informatiei/StopWords.cs
@@ -0,0 +1,52 @@
+namespace Regasirea_Informatiei;
+
+public class StopWords
+{ 
+    private readonly string _numeFisier = "StopWords.txt";
+    private List<string> _listaStopWords = new List<string>();
+        
+    public StopWords()
+    {
+            
+        using (var scriitor = new StreamWriter(_numeFisier, true))
+        {
+            bool fisierulExista = File.Exists(_numeFisier);
+            if (fisierulExista)
+            {
+                CitesteDate();
+            }
+            else
+            {
+                File.Create(_numeFisier);
+            }
+        }
+
+        Console.WriteLine("Fisier StopWords initializat.");
+        
+    }
+
+    public void CitesteDate()
+    {
+        using (StreamReader cititorCuvinte = new StreamReader(_numeFisier))
+        {
+            string[] cuvinte = cititorCuvinte.ReadToEnd().Split(' ');
+            foreach (string cuvant in cuvinte)
+            {
+                _listaStopWords.Add(cuvant);
+            }
+        }
+    }
+
+    public bool EsteStopWords(string cuvant)
+    {
+        if (_listaStopWords.Contains(cuvant))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    
+}
